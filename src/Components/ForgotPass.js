370fe57516e3bfc1d012a4125872369e81a63956
shubhamAW-pratiti <shubham.waykar@pratititech.com>
2023-08-07 10:48:52 +0530

    forgot password implement OTP based

--- src/Components/ForgotPass.js
+++ src/Components/ForgotPass.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import { Button, Container, Paper, TextField, Typography } from '@mui/material'
 
 const ForgotPass = () => {
+
+    const[email , setEmail]=useState('');
+    const[showOTPfield, setShowOTPField]=useState(false);
+    const[sendedOTP,setSendedOTP]=useState('');
+    const[otp , setOtp]=useState('');
+    const[submitOTPButton, setSubmitOTPButton]=useState(false);
+
+    const sendOTP=(value)=>{
+        setOtp(value);
+        alert("OTP is "+value);
+        setShowOTPField(true);
+        setSubmitOTPButton(true);
+    }
+
+    const handleEmailChange=(value)=>{
+        setEmail(value);
+        
+    }
+
+    const handleOTPChange=(value)=>{
+        setOtp(value);
+    }
+
+    const hanldePasswordReset=()=>{
+        const storedUser=JSON.parse(localStorage.getItem('user'));
+
+        if(storedUser && email===storedUser.email){
+            //OTP set
+           const generatedOTP= Math.floor(10000+Math.random()*900000);
+           console.log("Generated OTP",generatedOTP);
+           setSendedOTP(generatedOTP);
+
+           sendOTP(generatedOTP.toString());
+        }
+        else{
+            alert("Email does not Exist!");
+        }
+        
+    }
+
+    //handle submit otp
+    const handleSubmitOTP=()=>{
+        if(otp==sendedOTP){
+            alert("OTP validated successfully");
+        }
+        else{
+            alert("Wrong OTP entered!");
+        }
+    }
   return (
     <Container component='main' maxWidth='md'>
         <Paper
@@ -22,12 +71,32 @@ const ForgotPass = () => {
                     required
                     fullWidth
                     label='Email'
+                    onChange={(e)=>handleEmailChange(e.target.value)}
+                    disabled={showOTPfield}
+
                 />
+
+               {showOTPfield&&(
+                    <TextField
+                    type='number'
+                    required
+                    fullWidth
+                    label='OTP'
+                    onChange={(e)=>handleOTPChange(e.target.value)}
+                />
+               )} 
             
 
             {/* Send OTP */}
 
-            <Button fullWidth variant='contained' onClick={()=>"OTP sent to Your email"}>Send OTP</Button>
+            {!submitOTPButton && (
+                <Button fullWidth variant='contained' onClick={hanldePasswordReset}>Send OTP</Button>
+            )}
+
+            {submitOTPButton &&(
+                <Button fullWidth variant='contained'onClick={handleSubmitOTP}>Submit OTP</Button>
+            )}
+            
         </Paper>
 
     </Container>
