28b18db2f38d833f7f172172c584f572f789133b
shubhamAW-pratiti <shubham.waykar@pratititech.com>
2023-08-16 16:39:09 +0530

    added table in userprofile

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,43 +1,410 @@
-import React from 'react'
-import {useState , useEffect} from 'react';
-import { useParams } from 'react-router-dom';
+
+
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
+import {
+    Grid,
+    Typography,
+    Paper,
+    Avatar,
+    Table,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    TablePagination,
+    TextField,
+    InputAdornment,
+} from '@mui/material';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import CloseIcon from '@mui/icons-material/Close';
 
 const UserProfile = () => {
-    const {userId}=useParams();
-    const [user,setUser]=useState(null);
+    const { userId } = useParams();
+    const [user, setUser] = useState(null);
+    const [tickets, setTickets] = useState([]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [searchTerm, setSearchTerm] = useState('');
 
-    console.log('user id',userId);
-    //fetch user details when the component mounts
-    useEffect(()=>{
-        axios.get(`http://localhost:3002/user/${userId}`)
-        .then((response)=>{
-            if(response.status===200){
-                const fetchedUser=response.data.data;
-                setUser(fetchedUser);
-            }else{
-                console.log('problem with fetching user details');
-            }
-        }).catch((error)=>{
-            console.log('error fetching user details',error);
-        })
-    },[userId])
+    useEffect(() => {
+        // Fetch user details
+        axios
+            .get(`http://localhost:3002/user/${userId}`)
+            .then((response) => {
+                if (response.status === 200) {
+                    const fetchedUser = response.data.data;
+                    setUser(fetchedUser);
+                } else {
+                    console.log('Problem with fetching user details');
+                }
+            })
+            .catch((error) => {
+                console.log('Error fetching user details', error);
+            });
 
-  return (
-    <div
-        style={{
-            width:'100%',
-            height:'100%',
-            borderRadius: '10px',
-            backgroundColor: '#f5f5f5',
-            padding: '5%',
-        }}
-    >
-        <h4>User Profile</h4>
-        {console.log('user',user)}
-        <p>User id: {user?.email}</p>
-    </div>
-  )
-}
+        // Fetch tickets created by the user
+        axios
+            .get('http://localhost:3002/allTicketsByUser', {
+                params: {
+                    userId: userId,
+                },
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            })
+            .then((response) => {
+                if (response.status === 200) {
+                    const tickets = response.data;
+                    setTickets(tickets);
+                } else {
+                    console.log('Problem with fetching tickets');
+                }
+            })
+            .catch((error) => {
+                console.error('Error fetching tickets:', error);
+            });
+    }, [userId]);
 
-export default UserProfile
+    const handlePageChange = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleRowsPerPageChange = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const filteredTickets = tickets.filter((ticket) =>
+        ticket.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const pendingTickets = filteredTickets.filter((ticket) => ticket.status === 'pending');
+    const openedTickets = filteredTickets.filter((ticket) => ticket.status === 'opened');
+    const closedTickets = filteredTickets.filter((ticket) => ticket.status === 'closed');
+
+    return (
+        <Grid container columnGap={2} rowGap={2}>
+            <Grid item xs={12}>
+                <Paper elevation={3} style={{ padding: '1rem', borderRadius: '10px' }}>
+                    <Typography variant="h4" gutterBottom>
+                        User Profile
+                    </Typography>
+                </Paper>
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                {/* ... user profile section ... */}
+                <Paper elevation={3} style={{ padding: '1rem', borderRadius: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                    <Avatar
+                        src="https://www.w3schools.com/howto/img_avatar.png"
+                        alt="Avatar"
+                        sx={{ width: '60%', height: 'auto', borderRadius: '50%', marginBottom: '1rem' }}
+                    />
+
+                    <Typography variant="h6" gutterBottom>
+                        {user && user.email}
+                    </Typography>
+
+                    <Typography variant="h6" gutterBottom sx={{ color: 'grey' }}>
+                        {user && user.role}
+                    </Typography>
+                </Paper>
+            </Grid>
+
+            <Grid item xs={12} sm={3} md={3} lg={3}>
+                {/* ... ticket statistics section ... */}
+                <Typography variant='h4'>
+                    Tickets:
+                </Typography>
+                <Paper elevation={3} style={{ padding: '1rem', borderRadius: '10px' }}>
+                    <Grid container justifyContent="space-evenly" alignItems="center">
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                            <Typography variant="h6">{tickets.length}</Typography>
+                            <Typography variant="subtitle2">Total Tickets</Typography>
+                        </div>
+                        <div>
+                            <Typography variant="h7" >|</Typography>
+                        </div>
+
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                            <Typography variant="h6">{pendingTickets.length}</Typography>
+                            <Typography variant="subtitle2">Pending</Typography>
+                        </div>
+                        
+                        <div>
+                            <Typography variant="h7" >|</Typography>
+                        </div>
+
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                            <Typography variant="h6">{openedTickets.length}</Typography>
+                            <Typography variant="subtitle2">Opened</Typography>
+                        </div>
+
+                        <div>
+                            <Typography variant="h7" >|</Typography>
+                        </div>
+
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                            <Typography variant="h6">{closedTickets.length}</Typography>
+                            <Typography variant="subtitle2">Closed</Typography>
+                        </div>
+                    </Grid>
+                </Paper>
+            </Grid>
+
+            <Grid item xs={12} marginTop={4}>
+                <Typography variant="h5" gutterBottom style={{
+                    fontWeight: 'bold',
+                }}>
+                    Tickets Created by User
+                </Typography>
+                <Paper elevation={3} style={{ padding: '1rem', borderRadius: '10px' }}>
+                        <TextField
+                            type="text"
+                            placeholder="Search by title..."
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            style={{
+                                width: '100%',
+                                padding: '0.5rem',
+                                borderRadius: '5px',
+                                padding:'15px',
+                            }}
+                            InputProps={{
+                                startAdornment: (
+                                  <InputAdornment position="start">
+                                    <span role="img" aria-label="search">
+                                      🔍
+                                    </span>
+                                  </InputAdornment>
+                                ),
+                              }}
+                        />
+                    <TableContainer>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>ID</TableCell>
+                                    <TableCell>Status</TableCell>
+                                    <TableCell>Agent Assigned</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {filteredTickets.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((ticket) => (
+                                    <TableRow
+                                        key={ticket._id}
+                                        component={Link}
+                                        to={`/ticket/${ticket._id}`}
+                                        style={{ textDecoration: 'none', color: 'inherit' }}
+                                    >
+                                        <TableCell>{ticket?.title}</TableCell>
+                                        <TableCell>
+                                            {ticket.status === 'pending' ? (
+                                                <HelpOutlineIcon style={{ color: 'orange' }} />
+                                            ) : ticket.status === 'opened' ? (
+                                                <CheckCircleOutlineIcon style={{ color: 'green' }} />
+                                            ) : (
+                                                <CloseIcon style={{ color: 'red' }} />
+                                            )}
+                                        </TableCell>
+                                        <TableCell>{ticket.agent}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        component="div"
+                        count={filteredTickets.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handlePageChange}
+                        onRowsPerPageChange={handleRowsPerPageChange}
+                    />
+                </Paper>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default UserProfile;
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// import axios from 'axios';
+// import { Grid, Typography, Paper } from '@mui/material';
+// import Avatar from '@mui/material/Avatar';
+// import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+// import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+// import CloseIcon from '@mui/icons-material/Close';
+
+// const UserProfile = () => {
+//     const { userId } = useParams();
+//     const [user, setUser] = useState(null);
+//     const [tickets, setTickets] = useState([]);
+    
+//     const TotalTickets = tickets.length;
+
+//     useEffect(() => {
+//         // Fetch user details
+//         axios.get(`http://localhost:3002/user/${userId}`)
+//         .then((response) => {
+//             if (response.status === 200) {
+//                 const fetchedUser = response.data.data;
+//                 setUser(fetchedUser);
+//             } else {
+//                 console.log('Problem with fetching user details');
+//             }
+//         })
+//         .catch((error) => {
+//             console.log('Error fetching user details', error);
+//         });
+
+//         // Fetch tickets created by the user
+//         axios.get('http://localhost:3002/allTicketsByUser', {
+//             params: {
+//                 userId: userId,
+//             },
+//             headers: {
+//                 'Content-Type': 'application/x-www-form-urlencoded',
+//             },
+//         })
+//         .then((response) => {
+//             if (response.status === 200) {
+//                 const tickets = response.data;
+//                 setTickets(tickets);
+//             } else {
+//                 console.log('Problem with fetching tickets');
+//             }
+//         })
+//         .catch((error) => {
+//             console.error('Error fetching tickets:', error);
+//         });
+
+//     }, [userId]);
+
+//     const pendingTickets = tickets.filter((ticket) => ticket.status === 'pending');
+//     const openedTickets = tickets.filter((ticket) => ticket.status === 'opened');
+//     const closedTickets = tickets.filter((ticket) => ticket.status === 'closed');
+
+//     return (
+//         <Grid container columnGap={2} rowGap={2}>
+//             <Grid item xs={12}>
+//                 <Paper elevation={3} style={{ padding: '1rem', borderRadius: '10px' }}>
+//                     <Typography variant="h4" gutterBottom>
+//                         User Profile
+//                     </Typography>
+//                 </Paper>
+//             </Grid>
+
+//             <Grid item xs={12} sm={6} md={4} lg={3}
+//             >
+//                 <Paper elevation={3} style={{ padding: '1rem', borderRadius: '10px' ,
+//                 display: 'flex',
+//                 flexDirection: 'column',
+//                 alignItems: 'center',
+//                 justifyContent: 'center',
+
+//             }}>
+//                     <Avatar
+//                         src="https://www.w3schools.com/howto/img_avatar.png"
+//                         alt="Avatar"
+//                         sx={{ width: '60%', height: 'auto', borderRadius: '50%', marginBottom: '1rem' }}
+//                     />
+
+//                     <Typography variant="h6" gutterBottom>
+//                         {user && user.email}
+//                     </Typography>
+
+//                     <Typography variant="h6" gutterBottom sx={{ color: 'grey' }}>
+//                         {user && user.role}
+//                     </Typography>
+//                 </Paper>
+//             </Grid>
+
+//             <Grid item xs={12} sm={3} md={3} lg={3}>
+//                          <Typography variant='h4'>
+//                             Tickets:
+//                         </Typography>
+//                 <Paper elevation={3} style={{ padding: '1rem', borderRadius: '10px' }}>
+//                     <Grid container justifyContent="space-evenly" alignItems="center">
+                       
+//                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+//                             <Typography variant="h6">{TotalTickets}</Typography>
+//                             <Typography variant="subtitle2">Total Tickets</Typography>
+//                         </div>
+
+//                         <div>
+//                             <Typography variant="h7" >|</Typography>
+//                         </div>
+
+//                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+//                             <Typography variant="h6">{pendingTickets.length}</Typography>
+//                             <Typography variant="subtitle2">Pending</Typography>
+//                         </div>
+                        
+//                         <div>
+//                             <Typography variant="h7" >|</Typography>
+//                         </div>
+
+//                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+//                             <Typography variant="h6">{openedTickets.length}</Typography>
+//                             <Typography variant="subtitle2">Opened</Typography>
+//                         </div>
+
+//                         <div>
+//                             <Typography variant="h7" >|</Typography>
+//                         </div>
+
+//                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+//                             <Typography variant="h6">{closedTickets.length}</Typography>
+//                             <Typography variant="subtitle2">Closed</Typography>
+//                         </div>
+//                     </Grid>
+//                 </Paper>
+//             </Grid>
+//         </Grid>
+//     );
+// };
+
+// export default UserProfile;
+
+    
+//    // export default UserProfile;
+    
+//     // <div
+//     //     style={{
+//     //         width: '100%',
+//     //         height: '100%',
+//     //         borderRadius: '10px',
+//     //         backgroundColor: '#f5f5f5',
+//     //         padding: '5%',
+//     //     }}
+//     // >
+//     //     <h4>User Profile</h4>
+//     //     {user && (
+//     //         <div>
+//     //             <p>User id: {user.email}</p>
+//     //             <p>User role: {user.role}</p>
+//     //         </div>
+//     //     )}
+//     //     <h4>Tickets Created by User</h4>
+//     //     {tickets.length > 0 ? (
+//     //         <ul>
+//     //             {tickets.map((ticket) => (
+//     //                 <li key={ticket._id}>{ticket.title}</li>
+//     //             ))}
+//     //         </ul>
+//     //     ) : (
+//     //         <p>No tickets created by this user</p>
+//     //     )}
+//     // </div>
--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,18 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { List, ListItem } from '@mui/material';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Paper,
+  TextField,
+  InputAdornment,
+  FormControl,
+  Select,
+  MenuItem,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const Users = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [searchValue, setSearchValue] = useState('');
+  const [filterRole, setFilterRole] = useState('all');
+
+  const theme = useTheme();
+  const isMdScreen = useMediaQuery(theme.breakpoints.up('md'));
 
   useEffect(() => {
     axios
-      .get('http://localhost:3002/users') // Assuming your API endpoint is '/getUsers'
+      .get('http://localhost:3002/users')
       .then((response) => {
         if (response.status === 200) {
-          const fetchedUsers = response.data.data; // Assuming the response structure has a 'data' field containing users
+          const fetchedUsers = response.data.data;
           setUsers(fetchedUsers);
           setLoading(false);
         } else {
@@ -26,35 +49,132 @@ const Users = () => {
       });
   }, []);
 
+  const filteredUsers = users.filter(
+    (user) =>
+      user.email.toLowerCase().includes(searchValue.toLowerCase()) &&
+      (filterRole === 'all' || user.role === filterRole)
+  );
+
+  const handleChangePage = (_, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleFilterRole = (event) => {
+    setFilterRole(event.target.value);
+  };
+
   return (
-    <div>
-      <h4>Users</h4>
-      {console.log('users', users)}
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        users.map((user) => (
-         <ListItem key={user._id}>
-            <Link to={`/user/${user._id}`}
-            style={{
-              textDecoration: 'none',
-              color: 'black',
-              width: '100%',
-              height:'40px',
-              backgroundColor: '#f5f5f5',
-              border: '1px solid #ccc',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              padding: '16px',
-            }}
-            >
-              {/* here the box of user goes like photo , total ticket . */}
-              {user?.email}
-            </Link>
-          </ListItem>
-        ))
-      )}
+    <div style={{ padding: isMdScreen ? '20px' : '0' }}>
+      <h2>Users</h2>
+
+      {/* Search Bar */}
+      <TextField
+        label="Search by Email"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <span role="img" aria-label="search">
+                🔍
+              </span>
+            </InputAdornment>
+          ),
+        }}
+        fullWidth
+        margin="normal"
+      />
+
+      {/* Filter by Role */}
+      <FormControl>
+        <Select
+          value={filterRole}
+          onChange={handleFilterRole}
+          variant="outlined"
+          margin="normal"
+        >
+          <MenuItem value="all">All Roles</MenuItem>
+          <MenuItem value="admin">Admin</MenuItem>
+          <MenuItem value="agent">Agent</MenuItem>
+          <MenuItem value="user">User</MenuItem>
+        </Select>
+      </FormControl>
+
+      {/* Table */}
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead >
+            <TableRow  >
+              <TableCell
+                  style={{
+                    fontSize: '1.2rem',
+                    backgroundColor:'#f5f5f5'
+                  }}
+    
+              >
+                Email
+              </TableCell>
+              <TableCell
+                  style={{
+                    fontSize: '1.2rem',
+                    backgroundColor:'#f5f5f5'
+                  }}
+    
+              >
+                Role
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {loading ? (
+              <TableRow>
+                <TableCell colSpan={2}>Loading...</TableCell>
+              </TableRow>
+            ) : (
+              filteredUsers
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((user) => (
+                  <TableRow
+                    key={user._id}
+                    component={Link}
+                    to={`/user/${user._id}`}
+                    style={{
+                      textDecoration: 'none',
+                      color: 'black',
+                      transition: 'background-color 0.2s',
+                     
+                    }}
+                  >
+                    <TableCell
+                      style={{
+                        // hover 
+                        '&:hover': {
+                          backgroundColor: '#f5f5f5',
+                        }
+                      }}
+                    >{user.email}</TableCell>
+                    <TableCell>{user.role}</TableCell>
+                  </TableRow>
+                ))
+            )}
+          </TableBody>
+
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 50]}
+        component="div"
+        count={filteredUsers.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </div>
   );
 };
